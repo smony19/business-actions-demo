c1c45020f3521fc5aec8edfc5edcf22376e52126
smony19 <115245614+smony19@users.noreply.github.com>
2023-07-27 14:32:08 +0530

    add test workflow

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+/** @jest-environment jsdom */
+const {fetch} = require('whatwg-fetch');
+global.fetch = fetch;
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { App } from './App';
+
+it('should render', () => {
+  render(<App />);
+
+  expect(screen.getByText('Business Contacts')).toBeInTheDocument();
+});
+
+it('should not fail', () => {
+  expect(true).toBe(true);
+});
